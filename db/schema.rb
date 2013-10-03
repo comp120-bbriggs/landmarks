e2c5fc35e7ea4c5a555473b71e127543337017f7
Ben Briggs <ben.s.briggs@gmail.com>
2013-10-03 17:16:59 -0400

    changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131001213444) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "landmarks", force: true do |t|
     t.float    "longitude"
     t.float    "latitude"
